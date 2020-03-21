112a5457ef9d648cfd39582d33bbd09da235f180
stephenlai <stephenlai2015@gmail.com>
2020-03-21 13:37:08 +0800

    update README.md

--- parser.js
+++ parser.js
@@ -33,14 +33,20 @@ async function checkPrice() {
     // e.x. US$199.99 => 199
     const priceNumber = parseFloat(priceString.replace('US$', ''))
     console.log(`
-      current price: ${priceNumber} 
-      minimum price: ${minPrice}
+      - current price: ${priceNumber} 
+      - ideal price: ${minPrice}      
     `)
     if (priceNumber < minPrice) {
       await sendEmail(
         `Price is Low`,
         `The price on ${url} has dropped below ${minPrice}`
-      )  
+      )       
+      console.log(`
+        - a notified email has been sent to your account
+        - the price is below your budget, you can purchase it now !
+      `)
+    } else {
+      console.log(`the price is still higher than your budget !`)
     }
   } catch (e) {
     // 如果 Email 的程式碼有錯誤, 寄一封 Email 告知
@@ -48,6 +54,8 @@ async function checkPrice() {
     throw e
   }
 }
+
+// node parser.js https://www.amazon.com/Samsung-970-EVO-Plus-MZ-V7S1T0B/dp/B07MFZY2F2 200
 
 
 function sendEmail(subject, body) {
