f76741e2329d9d64c436e20ae397bcef70d4d70b
stephenlai <stephenlai2015@gmail.com>
2020-03-22 13:18:41 +0800

    clean app.js

--- app.js
+++ app.js
@@ -3,24 +3,13 @@ const app  = express()
 const engine = require('ejs-locals')
 const port = process.env.PORT || 3000
 const checkPrice = require('./check-current-price')
-const cors = require('cors')
 
 app.engine('ejs', engine)
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 
-app.use(cors())
 app.use(express.static(__dirname + '/client'))
 app.use(express.json())
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-})
 
 app.get('/',  (req, res) => {  
   res.render('index')  
