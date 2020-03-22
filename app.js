efdd7f9bd83b8c6be400f782e4f7d5090e136baf
stephenlai <stephenlai2015@gmail.com>
2020-03-22 11:58:27 +0800

    fix check-price function

--- app.js
+++ app.js
@@ -2,29 +2,24 @@ const express = require('express')
 const app  = express()
 const engine = require('ejs-locals')
 const port = process.env.PORT || 3000
-const checkPrice = require('./check-price')
-const bodyParser = require('body-parser')
+const checkPrice = require('./check-price-v2')
 
 app.engine('ejs', engine)
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 
-app.use(express.static('client'))
-// app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
 
-let myMessage = 'returned message...'
 app.get('/',  (req, res) => {  
-  res.render('index', { myMessage })  
+  res.render('index')  
 })
 
 app.post('/checkPrice', async (req, res) => {  
   const { price } = req.body
   console.log(`minPrice: ${price}`)
-  let { message } = await checkPrice(price)
-  console.log(`信息: ${message}`)
-  myMessage = message
-  res.status(200).redirect('/')
+
+  const { currentPrice } = await checkPrice()  
+  res.send({ price: currentPrice })
 })
 
 app.listen(port, () => {
---
+++ check-price-v2.js
@@ -0,0 +1,21 @@
+const nightmare = require('nightmare')()
+
+const url = "https://www.amazon.com/Samsung-970-EVO-Plus-MZ-V7S1T0B/dp/B07MFZY2F2"
+
+async function checkPrice() {
+  try {    
+    const priceString = await nightmare
+      .goto(url)
+      .wait('.a-color-price') 
+      .evaluate(() => document.querySelector('.a-color-price').innerText)
+      // .end() 
+    
+    const priceNumber = parseFloat(priceString.replace('US$', ''))   
+    console.log(`current price: ${priceNumber}`)
+    return { currentPrice: `${priceNumber}` }
+  } catch (e) { 
+    console.log(e) 
+  }
+}
+
+module.exports = checkPrice
